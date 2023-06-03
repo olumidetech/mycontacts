d5efafb007273c1adc58d7a81fcd4b00e3e1dea0
olumidtech <olumideadewusis@gmail.com>
2023-06-03 23:02:40 +0100

    setup model, contoller and routes

---
+++ controller/contactController.js
@@ -0,0 +1,56 @@
+const asyncHandler = require("express-async-handler");
+const Contact = require("../models/contactModel");
+//@desc GET all contacts
+//@route GET /api/contacts
+//@access public
+
+const getContacts = asyncHandler(async (req, res) => {
+    const contacts = await Contact.find();
+    res.status(200).json(contacts);
+});
+
+
+
+//@desc Create New contacts
+//@route POST /api/contacts
+//@access public
+
+const createContact =  asyncHandler(async (req, res) => {
+    console.log(req.body);
+    const { name, email, phone } = req.body;
+    if(!name || !email || !phone) {
+        res.status(400);
+        throw new Error("All fields are mandatory!");
+    }
+    const contact = await Contact.create({
+        name, email, phone,
+    });
+    res.status(201).json(contact);
+});
+
+//@desc GET  contacts
+//@route GET /api/contacts/:id
+//@access public
+
+const getContact =  asyncHandler(async (req, res) => {
+    const contact = await Con
+    res.status(200).json({ message: `Get contact for ${req.params.id}`});
+});
+
+//@desc Update  contacts
+//@route GET /api/contacts/:id
+//@access public
+
+const updateContact =  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Update contact for ${req.params.id}`});
+});
+
+//@desc Delete  contacts
+//@route Delete /api/contacts/:id
+//@access public
+
+const deleteContact =  asyncHandler(async (req, res) => {
+    res.status(200).json({ message: `Delete contact for ${req.params.id}`});
+});
+
+module.exports = { getContacts, createContact, getContact, updateContact, deleteContact };
